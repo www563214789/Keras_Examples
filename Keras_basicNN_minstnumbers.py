4c714208ef3c0a15ae10e8ce35eb811bf46292e4
www563214789 <www563214789@gmail.com>
2018-11-12 00:55:26 +0800

    Sort code and fix syntax error and recomment

--- Keras_basicNN_minstnumbers.py
+++ Keras_basicNN_minstnumbers.py
@@ -9,24 +9,26 @@ import numpy as np
 model=Sequential()
 
 #Step 2 Construct Neuron Network Layers
-#2.1 Input layer
-model.add(Dense(units=500,input_dim=784)) #Input layer:28*28=784(data input from minst dataset)
-model.add(Activation("relu")) #Activation function = tanh
-model.add(Dropout(0.5)) #Set 50% dropout rate to prevent overfitting
+#2.1 Add input layer
+model.add(Dense(units=256, input_dim=784, kernel_initializer='normal', activation='relu')) 
 
-#2.2 Dense layer
-model.add(Dense(units=500)) #Dense layer node=500,same as input layer output nodes 
-model.add(Activation("relu")) #Activation function = Tanh
-
-#2.3 Output layer
-model.add(Dense(units=10)) #Output Layer dimension is 10, means 10(represent number"0"-"9") classfication
-model.add(Activation("softmax"))
+#2.2 Add output layer
+model.add(Dense(units=10, kernel_initializer='normal', activation='softmax'))
 
 #Step 3 Compile
 #sgd=SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True) #Set Optimizer arguments, Lr(Learning Rate)...etc
-#model.compile(loss='categorical_crossentropy',optimizer='sgd', metrics=['accuracy']) #Use 交叉熵 as loss function
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) 
 
+#Deal with input data
+(x_train, y_train), (x_test, y_test) = mnist.load_data() #Use mnist tools from Keras to get datasets
+#The dimension of input dataset in minst is (num,28,28), We need to transfer dimension 28,28 -> 784(28*28) (3D->2D)
+x_train_2D = x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2]) #reshape( 資料筆數 , 目標維度數)
+x_train_normal = x_train_2D.astype('float32')/255
+x_test_2D = x_test.reshape(x_test.shape[0], x_test.shape[1]*x_test.shape[2])
+x_test_normal = x_test_2D.astype('float32')/255
+
+y_train_encoded = to_categorical(y_train)
+y_test_encoded = to_categorical(y_test)
 
 #Step 4 Trainning
     #.fit 參數
@@ -34,36 +36,18 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
     #epochs ：訓練次數
     #shuffle：是否把訓練集隨機打亂後再進行訓練
     #validation_split：拿出百分之多少用來做交叉驗證
-    #verbose：屏显模式 0：不输出  1：输出进度  2：输出每次的训练结果
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data() #Use mnist tools from Keras to get datasets
-#The dimension of input dataset in minst is (num,28,28), We need to transfer dimension 28,28 -> 784(28*28) (立體->平面)
-x_train=x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2])
-x_train = x_train.astype('float32')/255
-x_test=x_test.reshape(x_test.shape[0], x_test.shape[1]*x_test.shape[2])
-x_test = x_test.astype('float32')/255
-
-y_train=to_categorical(y_train)
-y_test=to_categorical(y_test)
+    #verbose： 0：不輸出  1：輸出進度  2：輸出每次的訓練結果
 
 #Start Trainning
-model.fit(x=x_train, y=y_train, batch_size=200, epochs=50, verbose=0, validation_split=0.3)
+model.fit(x=x_train_normal, y=y_train_encoded, batch_size=800, epochs=10, verbose=2, validation_split=0.2)
 
 #Evaluate for test datasets
-model.evaluate(x_test, y_test, batch_size=200, verbose=0)
+scores = model.evaluate(x_test_normal, y_test_encoded)  
+print()  
+print("\t[Info] Accuracy of testing data = {:2.1f}%".format(scores[1]*100.0))  
 
 #Step 5 Output
-print('Test set')
-scores = model.evaluate(x_test, y_test, batch_size=200, verbose=0)
-print(" ")
-print("The test loss is %f" % scores)
-result= model.predict(x_test,batch_size=200, verbose=0)
-
-result_max = np.argmax(result, axis = 1)
-test_max = np.argmax(y_test, axis = 1)
-
-result_bool = np.equal(result_max, test_max)
-true_num =  np.sum(result_bool)
-
-print(" ")
-print("The accuracy of the model is %f" % (true_num / len(result_bool)))
+X = x_test_normal[0:10,:]
+predictions = model.predict_classes(X)
+# get prediction result
+print(predictions)
