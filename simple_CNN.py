8c57c3ee0138d5f6ed8cf9aaa65d4724ae9c6b67
www563214789 <www563214789@gmail.com>
2018-11-14 14:24:47 +0800

    simple CNN based on keras

--- simple_CNN.py
+++ simple_CNN.py
@@ -1,8 +1,9 @@
 import keras
-from keras.datasets import mnist
+from keras.datasets import mnist #Use mnist dataset
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Activation
-from keras.layers.convolutional import Conv2D, MaxPooling2D #CNN 使用卷積層(Conv2D) 池化層(Pooling)
+#CNN use convolution layer(Conv2D) and Pooling layer(MaxPooling2D)
+from keras.layers.convolutional import Conv2D, MaxPooling2D 
 from keras.utils import np_utils
 
 #Set image width and height
@@ -13,15 +14,18 @@ img_width = 28
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 #input datatype of CNN model is different with simple neural network
+#[Channel] Mode for backend engine
 #Channel mode for Tensorflow (amount, height, width, color channel(RGB=3 gray=1))
 #Channel mode for Theano, Caffe (amount, color channel(RGB=3 gray=1), height, width)
-x_train_2D = x_train.reshape(x_train.shape[0], img_width, img_height, 1)
-x_test_2D = x_test.reshape(x_test.shape[0], img_width, img_height, 1)
-input_shape = (img_width, img_height, 1)
-
+x_train_2D = x_train.reshape(x_train.shape[0], img_height, img_width, 1)
+x_test_2D = x_test.reshape(x_test.shape[0], img_height, img_width, 1)
+input_shape = (img_height, img_width, 1)
+#Normalize value in 0-1 in float32
 x_train_2D = x_train_2D.astype('float32')/255
 x_test_2D = x_test_2D.astype('float32')/255
 
+#Encode labels to 0-9 in like 0000100000 means 4 (at 4th position of bit)
+#In trainning data ====> [Image]->[Label]
 y_train_encoded = keras.utils.to_categorical(y_train, 10)
 y_test_encoded = keras.utils.to_categorical(y_test, 10)
 
@@ -50,15 +54,20 @@ model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimi
 #Trainning
 train_history = model.fit(x_train_2D, y_train_encoded, batch_size=128, epochs=12, verbose=2, validation_data=(x_test_2D, y_test_encoded))
 
-#Evaluating
+#Evaluating accuracy rate and loss rate
 loss_rate, accu_rate = model.evaluate(x_test_2D, y_test_encoded, verbose=0)
 print('Test loss:', loss_rate)
 print('Test accuracy:', accu_rate)
 
+#Put test dataset in trained neural network and get predicted result
+x = x_test_2D[:20, :]
+prediction = model.predict_classes(X)
+print(prediction)
+
 #Saving model and weight
-#Model
 from keras.models import model_from_json
-json_string = model_to_json() #Get model architechture in json by model_to_json()
+#Model
+json_string = model.to_json() #Get model architechture in json by model_to_json()
 with open("model.config","w") as text_file:
     text_file.write(json_string) #Create file and use function .write(filename) to save the model
 #Weight
